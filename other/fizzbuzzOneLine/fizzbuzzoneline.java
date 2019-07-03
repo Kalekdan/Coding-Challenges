6dc449eba21b426f9d0a47cd81372e865163284a
Joseph Rickard <joerickard@hotmail.co.uk>
2019-07-03 15:48:18 +0100

    Updated fizzbuzzoneline to actually be one line

--- other/fizzbuzzOneLine/fizzbuzzoneline.java
+++ other/fizzbuzzOneLine/fizzbuzzoneline.java
@@ -2,18 +2,6 @@ public class fizzbuzzoneline {
 	//An attempt to do the fizzbuzz algorithm in a single line
 	//Obviously this is terrible programming so don't do this!
 	public static void main(String[] args){
-		fizzBuzzToVal(100);
-	}
-	
-	private static void fizzBuzzToVal(int val){
-		String outStr;
-		for (int i = 1; i <= val; i++){
-			outStr = oneLineFizzIt(i);
-			System.out.println(outStr);
-		}
-	}
-	
-	private static String oneLineFizzIt(int toFizzBuzz){
-		return (toFizzBuzz % 3 == 0) ? (toFizzBuzz % 5 == 0) ? "FizzBuzz" : "Fizz" : (toFizzBuzz % 5 == 0) ? "Buzz" : String.valueOf(toFizzBuzz);
+		for(int i = 1; i <= 100; i++){ System.out.println((i % 3 == 0) ? (i % 5 == 0) ? "FizzBuzz" : "Fizz" : (i % 5 == 0) ? "Buzz" : String.valueOf(i));}
 	}
 }
